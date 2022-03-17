5e7656545a18a336894b34aa0ac7cb12de09a62d
Pranav Rajan <rajan.pranav320@gmail.com>
2022-03-17 07:41:29 -0600

    add tickets stuff

--- Makefile
+++ Makefile
@@ -185,6 +185,7 @@ UPROGS=\
 	_testexit\
 	_crc32\
 	_spin\
+	_tickets\
 
 fs.img: mkfs README $(UPROGS)
 	./mkfs fs.img README $(UPROGS)
@@ -253,7 +254,7 @@ qemu-nox-gdb: fs.img xv6.img .gdbinit
 EXTRA=\
 	mkfs.c ulib.c user.h cat.c echo.c forktest.c grep.c kill.c\
 	ln.c ls.c mkdir.c rm.c stressfs.c usertests.c wc.c zombie.c\
-	printf.c umalloc.c freemem.c testexit.c crc32.c spin.c\
+	printf.c umalloc.c freemem.c testexit.c crc32.c spin.c tickets.c\
 	README dot-bochsrc *.pl toc.* runoff runoff1 runoff.list\
 	.gdbinit.tmpl gdbutil\
 
---
+++ tickets.c
@@ -0,0 +1,26 @@
+
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+#include "pstat.h"
+
+// User program to test lottery scheduler
+int main(int argc, char *argv[])
+{
+    if (argc != 3)
+    {
+        printf(1, "Error: Invalid number of arguments for lottery scheduler\n");
+        exit();
+    }
+
+    // negative number check
+    if (argv[1][0] == '-')
+    {
+        printf(1, "Error: Invalid number of tickets. Negative tickets passed as argument\n");
+    }
+
+    int num_tickets = atoi(argv[1]); // the number of tickets the process should run with
+    settickets(num_tickets);
+    exec(argv[2], &argv[2]);
+    exit();
+}
