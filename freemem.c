8e98b0384b7ef3b88f506d78315ecefb82d50d11
Pranav Rajan <rajan.pranav320@gmail.com>
2022-03-17 07:11:34 -0600

    Fix the freemem print statement

--- freemem.c
+++ freemem.c
@@ -1,12 +1,12 @@
 #include "types.h"
 #include "stat.h"
 #include "user.h"
-
+#include "memlayout.h"
 int main(int argc, char *argv[])
 {
-    int i;
-    printf(1, "the freemem value from freemem %d\n", freemem());
-    for (i = 1; i < argc; i++)
-        printf(1, "%s%s", argv[i], i + 1 < argc ? " " : "\n");
+    //int i;
+    //printf(1, "freemem()%d\n", freemem());
+    //printf(1, "PHYSTOP: %d\n", PHYSTOP);
+    printf(1, "%d\n", freemem());
     exit();
 }
