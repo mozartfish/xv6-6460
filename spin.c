d5ea2727f82bfa1e2d73d5cdeb3cf1ed84cefd53
Pranav Rajan <rajan.pranav320@gmail.com>
2022-03-17 07:19:23 -0600

    Add in a spin program to test CPU for lottery scheduler and ps

---
+++ spin.c
@@ -0,0 +1,12 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+// this function runs an infinite loop for testing the scheduler
+int main(void)
+{
+    // infinite loop for spin
+    while (1)
+    {
+    }
+}
